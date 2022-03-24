0d9479db46a34b6fcb6eb29a475072d03f7373b8
VinVulpes <vinogradov.vulpes@gmail.com>
2022-03-24 11:59:00 +0300

    First

--- Diplom.py
+++ Diplom.py
@@ -1,2 +1,16 @@
 import pyparsing
-file = open("C:/Users/mi/Desktop/Diplom/xrun_register_rw.log")
+
+file = open("Test.log", 'r')
+# file = open(input("Введите путь к файлу: "))#выбор журнала для загрузки
+# Анализ и разбор сообщений журнала.
+for line in file:
+    print(line, end='')
+
+type_mes = World(['UVM_INFO', 'UVM_WARNING', ' UVM_ERROR', 'UVM_FATAL', 'OTHER'])
+file_path = World(alphas + ":" + "/")  # путь к файлу
+line_num = World("(" + nums + ")")  # номер строки
+time = World("@" + nums + ":")  # время появления сообщения в фс
+cause = World(alphas)  # !тут еще всякие @ _ глнянь# название тестовой последовательности
+prefix = World("[" + alphas + "]")  # префикс сообщения
+message = World(alphas)  # ! еще цифры в сообщении# сообщение
+table = type_mes + file_path + line_num + time + cause + prefix + message  # таблица со всеми данными
