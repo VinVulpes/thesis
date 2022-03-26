321a32236aeef43fa077446b6a228b0613deee45
VinVulpes <vinogradov.vulpes@gmail.com>
2022-03-26 22:54:16 +0300

    Worked with file(test3, with UVM, but no dict yet)

--- Diplom.py
+++ Diplom.py
@@ -1,6 +1,8 @@
 from pyparsing import *
 
-file = open("test.log", 'r')
+file = open("xrun_random_test.log", 'r')
+#file = open("xrun_register_rw.log", 'r')
+#file = open("test.log", 'r')
 '''
 test = [
     "UVM_INFO /home/user16/workspace/PROJ/sim/standalone_sim/cdns_apb_vip_efuse/apb_vip_src/cdnApbUvmUserMonitor.sv(76) @ 8898000: uvm_test_top.ApbSve0.myUvmEnv.passiveSlave0.monitor [MONITOR] Ended WRITE Transfer: Address = h'8  Data = h'fff",
@@ -15,8 +17,19 @@ file_path = Word(alphas + "/" + nums + "_" + ".")  # путь к файлу
 line_num = Suppress('(') + Word(nums) + Suppress(')')  # номер строки
 time = Suppress('@') + Word(' ' + nums) + Suppress(':')  # время появления сообщения в фс
 cause = Word(alphas + "." + "_" + nums + "@")  # !тут еще всякие @ _ глнянь# название тестовой последовательности
-prefix = Suppress('[') + Word(alphas) + Suppress(']')  # префикс сообщения
-message = Word(alphas + ":" + "'" + nums + "." + "=" + " " + "(" + ")" + ",")  # ! еще цифры в сообщении# сообщение
+prefix = Suppress('[') + Word(alphas + ":") + Suppress(']')  # префикс сообщения
+message = Word(
+    alphas + ":" + "'" + nums + "." + "=" + " " + "(" + ")" + "," + "-" + "*")  # ! еще цифры в сообщении# сообщение
 text = ZeroOrMore(type_mes + file_path + line_num + time + cause + prefix + message)
+info = 0
+m = 0
 for line in file:
-    print(text.parseString(line))
+    if line.startswith("UVM_INFO /") or line.startswith("UVM_WARNING /") or line.startswith(
+            "UVM_ERROR /") or line.startswith("UVM_FATAL /") or line.startswith("OTHER /"):
+        pr = text.parseString(line)
+        print(pr)
+        if (pr[0] == 'UVM_INFO'):
+            info += 1
+        if (pr[5] == 'MONITOR'):
+            m += 1
+print('UVM_INFO = ',info,'MONITOR = ',m)
--- proba.py
+++ proba.py
@@ -1,11 +1,6 @@
-from pyparsing import Word, Literal, alphas,ZeroOrMore,Suppress
-
-source = "a, b, c,d"
-wd = Word(alphas)
-wd_list1 = wd + Word(',' + wd)
-print(wd_list1.parseString(source))
-
- # often, delimiters that are useful during parsing are just in the
- # way afterward - use Suppress to keep them out of the parsed output
-wd_list2 = wd + ZeroOrMore(Suppress(',') + wd)
-print(wd_list2.parseString(source))
+from pyparsing import *
+a = 3
+b = 'hello'
+c = False
+r = None
+print(type(a), type(b), type(c),type(r))
