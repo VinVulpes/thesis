08dce9ea7d967f1b52234c8c3f4cdbed6cb15e36
VinVulpes <vinogradov.vulpes@gmail.com>
2022-03-26 15:59:45 +0300

    Worked with file(test2, with noUvm)

--- Diplom.py
+++ Diplom.py
@@ -1,6 +1,6 @@
 from pyparsing import *
 
-file = open("Test.log", 'r')
+file = open("test.log", 'r')
 '''
 test = [
     "UVM_INFO /home/user16/workspace/PROJ/sim/standalone_sim/cdns_apb_vip_efuse/apb_vip_src/cdnApbUvmUserMonitor.sv(76) @ 8898000: uvm_test_top.ApbSve0.myUvmEnv.passiveSlave0.monitor [MONITOR] Ended WRITE Transfer: Address = h'8  Data = h'fff",
@@ -11,13 +11,12 @@ test = [
     "UVM_WARNING /home/user16/workspace/PROJ/sim/standalone_sim/cdns_apb_vip_efuse/apb_vip_src/cdnApbUvmUserMonitor.sv(76) @ 10732858000: uvm_test_top.ApbSve0.myUvmEnv.activeMaster.monitor [MONITOR] Ended WRITE Transfer: Address = h'0  Data = h'300002a"]
 '''
 type_mes = Word("UVM_INFO" + "UVM_WARNING" + "UVM_ERROR" + "UVM_FATAL" + "OTHER")
-file_path = Word(alphas + "/" + nums + "_" + ".") # путь к файлу
-
-line_num = Suppress('(')+Word(nums)+Suppress(')')  # номер строки
-time = Suppress('@')+Word(' '+ nums)+Suppress(':')  # время появления сообщения в фс
+file_path = Word(alphas + "/" + nums + "_" + ".")  # путь к файлу
+line_num = Suppress('(') + Word(nums) + Suppress(')')  # номер строки
+time = Suppress('@') + Word(' ' + nums) + Suppress(':')  # время появления сообщения в фс
 cause = Word(alphas + "." + "_" + nums + "@")  # !тут еще всякие @ _ глнянь# название тестовой последовательности
-prefix = Suppress('[')+Word( alphas)+Suppress(']')  # префикс сообщения
+prefix = Suppress('[') + Word(alphas) + Suppress(']')  # префикс сообщения
 message = Word(alphas + ":" + "'" + nums + "." + "=" + " " + "(" + ")" + ",")  # ! еще цифры в сообщении# сообщение
 text = ZeroOrMore(type_mes + file_path + line_num + time + cause + prefix + message)
-print(text.parseFile(file))
-
+for line in file:
+    print(text.parseString(line))
